808144088d9e77ba13cce82515c84a3468fee784
Jonathan Wu <jwu20@stuy.edu>
2020-10-23 18:14:09 -0400

    Added largest() method

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,19 +1,30 @@
-public static int sum(intP{} arr) {
+public class ArrayOps {
+  public static int sum(int[] arr) {
+    int totalSum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      totalSum += arr[i];
+    }
+    return totalSum;
+  }
 
-}
-
-public static int largest(int[] arr) {
-
-}
-
-public static int[] sumrows(int[][] matrix) {
-
-}
-
-public static int[] largestInRows(int[][] matrix) {
-
-}
-
-public static int sum(int[][] arr) {
+  public static int largest(int[] arr) {
+    int largestInt = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > largestInt) largestInt = arr[i];
+    }
+    return largestInt;
+  }
+  //
+  // public static int[] sumrows(int[][] matrix) {
+  //
+  // }
+  //
+  // public static int[] largestInRows(int[][] matrix) {
+  //
+  // }
+  //
+  // public static int sum(int[][] arr) {
+  //
+  // }
 
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester {
   public static void main(String[] args) {
-
+    int[]arrayX = {1,20,3,43,5};
+    System.out.println(ArrayOps.largest(arrayX));
   }
 }
